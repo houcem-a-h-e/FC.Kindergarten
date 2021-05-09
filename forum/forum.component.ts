ee137024ab2330741bca2b33ae71342010b1968e
houcem-a-h-e <79806648+houcem-a-h-e@users.noreply.github.com>
2021-05-09 07:14:58 +0100

    Add files via upload

---
+++ forum/forum.component.ts
@@ -0,0 +1,101 @@
+import { Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventEmitter } from 'events';
+import { delay, timeout } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommentServiceService } from '../../services/comment-service.service';
+import { PostService } from '../extra-components/services/post.service';
+import { Parent, ParentType } from '../model/parent';
+import { Post } from '../model/post';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'ngx-forum',
+  templateUrl: './forum.component.html',
+  styleUrls: ['./forum.component.scss'],
+})
+export class ForumComponent implements OnInit {
+  idp: number;
+  idpostsearched: number;
+  idPostToRemove: number;
+  idPostToUpdate: number;
+
+  r: Router;
+  isShown: boolean = false;
+  shownOnAddPost: boolean = false;
+  hideMessageAfterTheAddPost: boolean = true;
+  list: Post[];
+  listofLikes: number[];
+  s: string = 'added successfully';
+  post: Post;
+  parent: Parent = {
+  id : 2  ,
+  firstName: 'houcem',
+  lastName : 'aouissaoui',
+  phonenumber : '2222222',
+  email : 'jcnscd@klsnd.com',
+  password : 'jarreb',
+  pType : ParentType.ordinaryParent,
+  };
+  display = 'none';
+  c: Comment;
+  comments: Comment[];
+  myDate: Date = new Date;
+  constructor(
+      private postSer: PostService,
+      private commentSer: CommentServiceService,
+    private modalService: NgbModal,
+    private fb: FormBuilder) { }
+  ngOnInit(): void {
+    // this.postSer.getPost().subscribe(() => (this.listofLikes.push(this.post.idPost)));
+    this.post = new Post();
+    // this.postSer.getPost().subscribe((data: any) => this.list = data);
+    this.getAllPosts();
+  }
+  ajoutPost() {
+    this.postSer.addPost(this.parent.id, this.post).subscribe(() => {
+      this.shownOnAddPost = true;
+      this.post.object_post = ' ';
+      this.post.textPost = ' ';
+      setTimeout(() => this.shownOnAddPost = false, 5000);
+      this.getAllPosts();
+    });
+  }
+
+  getAllPosts() {
+   this.postSer.getPost().subscribe((data: any) => this.list = data);
+  }
+  updatePostByClick() {
+
+  }
+
+  removePostByClick(idPostToRemove): void {
+    // let j = this.list.indexOf(this.post);
+    /*const index = this.list.findIndex(() => this.post.idPost === this.idPostToRemove);*/
+    this.postSer.removePostbyClick
+      (idPostToRemove).subscribe
+      (data => {
+        console.log(data);
+        this.getAllPosts();
+      });
+        // this.list.filter(post => post.idPost !== idPostToRemove));
+  }
+
+  getSepecificPost(idpostsearched) {
+    this.postSer.getPostById(this.idpostsearched).subscribe((data: Post) => this.post = data);
+    this.isShown = true;
+  }
+
+  openModal(event: any) {
+    // this.idpostsearched = this.idPostToRemove;
+    this.display = 'block';
+    // this.postSer.getPostById(idpostsearched).subscribe((data: Post) => this.post = data);
+    // console.log(idpostsearched);
+  }
+
+  onCloseHandled() {
+    this.display = 'none';
+  }
+
+}
---
+++ forum/forum.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import {
+  NbActionsModule,
+    NbAutocompleteDirective,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbDatepickerModule, NbIconModule,
+  NbInputModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbUserModule,
+} from '@nebular/theme';
+
+import { ThemeModule } from '../../@theme/theme.module';
+import { FormsRoutingModule } from './forum-routing.module';
+import { ForumComponent } from './forum.component';
+import { FormsModule as ngFormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    ThemeModule,
+    NbInputModule,
+    NbCardModule,
+    NbButtonModule,
+    NbActionsModule,
+    NbUserModule,
+    NbCheckboxModule,
+    NbRadioModule,
+    NbDatepickerModule,
+    NbSelectModule,
+    NbIconModule,
+    ngFormsModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [
+    ForumComponent,
+    /*FormsComponent,
+    ButtonsComponent,
+    FormInputsComponent,
+    FormLayoutsComponent,
+    DatepickerComponent,*/
+  ],
+})
+export class FormsModule { }
